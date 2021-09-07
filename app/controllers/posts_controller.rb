0165655411646dd477abcdc521bdc2c411398c09
Lincoln Anders <lincolnanders5@gmail.com>
2021-09-07 13:27:11 -0400

    add posts with images

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+  # Make ActiveStorage aware of the current host (used in url helpers)
+  include ActiveStorage::SetCurrent
+
+  before_action :set_post, only: [:show, :update, :destroy, :images]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  def images
+    render json: { url: @post.images.map(&:url) } if @post.images.attached?
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body, images: [])
+  end
+end
